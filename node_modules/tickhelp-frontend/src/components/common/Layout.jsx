9e7e71b554349bbb6f0cc998d49fa96789fed64d
Reine Enzo <enzo.reine@ecoles-epsi.net>
2025-06-18 11:55:30 +0200

    restart from almost scratch

--- node_modules/tickhelp-frontend/src/components/common/Layout.jsx
+++ node_modules/tickhelp-frontend/src/components/common/Layout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import Sidebar from './Sidebar';
+// import Header from './Header';
+// import Sidebar from './Sidebar';
 
 const Layout = () => {
   return (
     <div className="app-layout">
-      <Header />
+      {/* <Header /> */}
       <div className="main-content">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <main className="content-area">
           <Outlet />
         </main>
